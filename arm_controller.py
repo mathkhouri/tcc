34528f2814932f2404411edfa5c95326938cf757
mathkhouri <30637015+mathkhouri@users.noreply.github.com>
2021-06-05 18:36:03 -0300

    Update arm_controller.py
    Código com funcionalidades principais finalizado.

--- arm_controller.py
+++ arm_controller.py
@@ -10,9 +10,9 @@ from time import sleep
 from typing import List
 
 IP = '127.0.0.1'
-PORT = 57120
-SPEED = 1.0
-ARM_LENGTH = 0.5 / 3  # Em metros
+PORT = 9000
+ARM_LENGTH = 0.5  # Em metros
+MAX_VELOCITY = 3.14
 PI = 3.14
 
 BPM = 120
@@ -26,7 +26,7 @@ SEMICOLCHEIA = 16
 FUSA = 32
 SEMIFUSA = 64
 
-INITIAL_NOTES = [4, 4, 8, 8, 4]
+NOTES = [4, 4, 8, 8, 4, 4]
 
 STATUS = {
     0: "down command",
@@ -37,15 +37,10 @@ STATUS = {
 }
 
 UP_POSITION = -1.5
-DOWN_POSITION = -2.8
+DOWN_POSITION = 0.0
 
 
-def set_velocity(ur_motors):
-    for motor in ur_motors:
-        Motor.setVelocity(motor, SPEED)
-
-
-def define_time_signature_duration() -> float:
+def get_time_signature_duration() -> float:
     """Retorna a duração em segundos de cada batida, de acordo com o BPM definido para a música
 
     :return: float representando a duração em segundos de cada unidade de tempo de um compasso
@@ -53,18 +48,44 @@ def define_time_signature_duration() -> float:
     return 60 / BPM
 
 
-def get_notes_duration(time_signature_duration, *notes) -> List[float]:
+def check_pause(note) -> int:
+    return int(note / 10) if note % 10 == 0 else note
+
+
+def calculate_note_duration(note):
+    return get_time_signature_duration() * (TIME_SIGNATURE[1] / check_pause(note))
+
+
+def get_notes_duration(*notes) -> List[float]:
     """De acordo com o BPM escolhido, calcula (em segundos) quanto tempo irá durar cada nota, de acordo com seu tipo.
 
     :param float time_signature_duration: duração em segundos de cada unidade de tempo de um compasso
     :param int notes: lista contendo o valor representativo de cada nota a ser tocada (em última instância, da música)
     :return: lista contendo todos os tempos, em segundos, que irão durar cada a nota a ser tocada
     """
-    return [time_signature_duration * (TIME_SIGNATURE[1] / n) for n in notes]
+    return [calculate_note_duration(n) for n in notes]
 
 
-def calculate_velocity(total_duration):
-    return (2 * PI * ARM_LENGTH / 4) / (total_duration / 2)
+def calculate_note_vs_distance_factor(note) -> float:
+    return min((TIME_SIGNATURE[1] / note), 1.0)
+
+
+def calculate_linear_distance(initial_position, final_position, note_vs_distance_factor, half_note_duration) -> float:
+    return min((2 * PI * ARM_LENGTH * (abs(initial_position - final_position) / 6.28)) * note_vs_distance_factor,
+               MAX_VELOCITY * ARM_LENGTH * half_note_duration)
+
+
+def calculate_angular_velocity(linear_distance, half_note_duration) -> float:
+    return (linear_distance / half_note_duration) / ARM_LENGTH
+
+
+def get_angular_velocity(total_note_duration, note, sensor_position) -> tuple:
+    note_vs_distance_factor = calculate_note_vs_distance_factor(note)
+    linear_distance = calculate_linear_distance(sensor_position, UP_POSITION, note_vs_distance_factor,
+                                                total_note_duration / 2)
+    speed = calculate_angular_velocity(linear_distance, total_note_duration / 2)
+    up_position = -1 * (linear_distance / ARM_LENGTH)
+    return speed, up_position
 
 
 def init_robot():
@@ -110,44 +131,52 @@ def init_sensors(robot, timestep):
 
 
 def play_notes(robot, timestep, client, hand_motors, ur_motors, distance_sensor, touch_sensor, position_sensor):
-    time_signature_duration = define_time_signature_duration()
-    notes = get_notes_duration(time_signature_duration, *INITIAL_NOTES)
-    status = 0
-    status_dict = STATUS.copy()
+    current_status = 0
     notes_counter = 0
-    # Move o braço para posição inicial - 90º
-    Motor.setPosition(ur_motors[0], UP_POSITION)
+    notes = NOTES.copy()
+    status_dict = STATUS.copy()
+    notes_durations = get_notes_duration(*notes)
+    speed, up_position = get_angular_velocity(get_time_signature_duration(), TIME_SIGNATURE[1], DOWN_POSITION)
+    Motor.setVelocity(ur_motors[0], speed)
+    Motor.setPosition(ur_motors[0], up_position)
     while robot.step(timestep) != -1:
-        if status_dict.get(status) == "down command":
-            if PositionSensor.getValue(position_sensor) > UP_POSITION + 0.1:
-                print(status_dict.get(status))
-                if notes_counter >= len(notes):
+        if status_dict.get(current_status) == "down command":
+            if PositionSensor.getValue(position_sensor) < up_position + 0.1:
+                print(status_dict.get(current_status))
+                if notes_counter >= len(notes_durations):
                     break
-                total_duration = notes[notes_counter]
-                speed = calculate_velocity(total_duration)
-                # for ur_motor, position in zip(ur_motors, target_position):
-                Motor.setVelocity(ur_motors[0], speed)
-                Motor.setPosition(ur_motors[0], DOWN_POSITION)
-                status = 1
+                elif notes[notes_counter] % 10 == 0:
+                    current_status = 4
+                else:
+                    Motor.setPosition(ur_motors[0], DOWN_POSITION)
+                    current_status = 1
 
-        elif status_dict.get(status) == "going down":
-            if PositionSensor.getValue(position_sensor) < DOWN_POSITION + 0.1:
-                print(status_dict.get(status))
+        elif status_dict.get(current_status) == "going down":
+            if PositionSensor.getValue(position_sensor) > DOWN_POSITION - 0.1:
+                print(status_dict.get(current_status))
                 touch_value = TouchSensor.getValue(touch_sensor)
                 print("Detecting a collision of:", round(touch_value, 2), "N")
-                client.send_message("/controllers/robotic_arm", touch_value)
-                status = 2
+                client.send_message("/osc_signal_receiver", touch_value)
+                current_status = 2
 
-        elif status_dict.get(status) == "up command":
-            print(status_dict.get(status))
-            Motor.setPosition(ur_motors[0], UP_POSITION + 0.2)
-            status = 3
+        elif status_dict.get(current_status) == "up command":
+            print(status_dict.get(current_status))
+            speed, up_position = get_angular_velocity(notes_durations[notes_counter], notes[notes_counter],
+                                                      PositionSensor.getValue(position_sensor))
+            Motor.setVelocity(ur_motors[0], speed)
+            Motor.setPosition(ur_motors[0], up_position)
+            current_status = 3
 
-        elif status_dict.get(status) == "going up":
-            if PositionSensor.getValue(position_sensor) > UP_POSITION + 0.1:
-                print(status_dict.get(status))
-                status = 0
+        elif status_dict.get(current_status) == "going up":
+            if PositionSensor.getValue(position_sensor) < up_position + 0.1:
+                print(status_dict.get(current_status))
+                current_status = 0
                 notes_counter += 1
+
+        elif status_dict.get(current_status) == 'waiting pause':
+            notes.pop(notes_counter)
+            sleep(notes_durations.pop(notes_counter))
+            current_status = 0
 
 
 def start():

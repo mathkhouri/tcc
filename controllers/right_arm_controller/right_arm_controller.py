0f0715af243fad0b6bd81d29f86e85f752647fda
mathkhouri <math_khouri@hotmail.com>
2021-06-14 19:33:31 -0300

    Todas as funções comentadas e documentadas

--- controllers/right_arm_controller/right_arm_controller.py
+++ controllers/right_arm_controller/right_arm_controller.py
@@ -15,7 +15,7 @@ from time import sleep
 IP = '127.0.0.1'
 PORT = 9000
 ARM_LENGTH = 0.5  # Em metros
-MAX_VELOCITY = 3.14
+MAX_VELOCITY = 3.14  # Em rad/s
 PI = 3.14
 
 BPM = 115
@@ -69,11 +69,15 @@ STATUS = {
     4: 'waiting pause',
 }
 
-UP_POSITION = -1.5
-DOWN_POSITION = 0.0
+UP_POSITION = -1.5  # Maxima posicao do braco, em rad
+DOWN_POSITION = 0.0  # Minima posicao do braco, em rad
 
 
 def wait_pause(time_delta):
+    """Espera o tempo de pausa da nota a partir do parâmetro recebido. Tem como função parar o movimento do braço.
+
+    :param float time_delta: tempo de pausa a ser esperado
+    """
     initial_time = perf_counter()
     while True:
         if perf_counter() - initial_time >= time_delta:
@@ -81,6 +85,13 @@ def wait_pause(time_delta):
 
 
 def thread_pause(notes, notes_counter, notes_durations):
+    """Executada por uma thread, chama a função responsável pela pausa para N pausas seguidas, retornando apenas quando
+    todas as pausas tiverem terminado.
+
+    :param list notes: lista contendo todas as notas da música, para que N sucessivas pausas sejam respeitadas
+    :param int notes_counter: contador indicando qual é a nota atual da lista `notes`
+    :param list notes_durations: lista contendo as durações, em segundos, de todas as notas da música
+    """
     try:
         while notes[notes_counter] % 10 == 0:
             notes.pop(notes_counter)
@@ -98,10 +109,20 @@ def get_time_signature_duration() -> float:
 
 
 def check_pause(note) -> int:
+    """Checa se a nota recebida pertence ao padrão de uma pausa ou de uma nota a ser tocada. Retorna seu valor absoluto
+
+    :param int note: nota para ser analizada
+    :return: valor absoluto da nota, independente se for uma representação de pausa ou de nota a ser tocada
+    """
     return int(note / 10) if note % 10 == 0 else note
 
 
-def calculate_note_duration(note):
+def calculate_note_duration(note) -> float:
+    """Calcula a duração, em segundos, da nota recebida. Utiliza o BPM e a fórmula de compasso definidos.
+
+    :param int note: código da nota a ser calculado o tempo de duração
+    :return: tempo de duração real da nota, calculado em segundos
+    """
     return get_time_signature_duration() * (TIME_SIGNATURE[1] / check_pause(note))
 
 
@@ -116,19 +137,66 @@ def get_notes_duration(*notes) -> List[float]:
 
 
 def calculate_note_vs_distance_factor(note) -> float:
+    """Calcula o fator de correção normalizado entre a nota recebida e a distância angular de movimetnação do braço.
+
+    Para que a máxima distância angular do braço se mantenha limitada pelo valor definido em `UP_POSITION`, o máximo
+    valor retornado é 1. Dessa forma, notas que duram mais que a unidade de tempo não irão considerar uma excursão
+    do braço maior que o próprio `UP_POSITION`, pois irão compensar a duração da nota diminuindo a velocidade do braço.
+    Para notas que possuem duração menor do que a unidade de tempo, será feita uma porcentagem para que o braço se
+    movimente proporcionalmente à duração da nota. Ou seja, se a nota recebida possuir metade da duração da unidade de
+    tempo, a excursão do braço será metade da total e assim por diante.
+
+    Portanto, considerando um cenário de fórmula de compasso 4, 4, temos os seguintes exemplos:
+        - Nota recebida: 4  ->  valor retornado: 1
+        - Nota recebida: 2  ->  valor retornado: 1
+        - Nota recebida: 8  ->  valor retornado: 0.5
+
+    :param int note: nota a ser tocada
+    :return: fator de propoção normalizado do movimento do braço
+    """
     return min((TIME_SIGNATURE[1] / note), 1.0)
 
 
 def calculate_linear_distance(initial_position, final_position, note_vs_distance_factor, half_note_duration) -> float:
+    """Calcula a distância linear necessária para que o movimento do braço respeite o tempo da nota e a velocidade max.
+
+    Realia dois cálculos distintos para a distância linear:
+        - Excursão máxima do braço multiplicada pela fator de correção nota X distância angular;
+        - Setando a velocidade do motor a 3.14 rad/s;
+
+    Dessa forma, é utilizado o menor valor encontrado dentre as duas opções, pois caso o primeiro método resulte no
+    maior valor, significa que a máxima velocidade do motor seria desrespeitada. Entretanto, se o segundo método possuir
+    o maior valor, significa que não é necessário utilizar a máxima velocidade do motor, pois é possível atingir o mesmo
+    tempo apenas reduzindo a velocidade e aumentando a distância.
+
+    :param float initial_position: posição inicial do braço
+    :param float final_position: posição final do braço
+    :param float note_vs_distance_factor: fator de correção nota X distância angular
+    :param float half_note_duration: metade do tempo total de duração da nota
+    :return: distância linear a ser percorrida pelo braço, respeitando o tempo de duração da nota e a velocidade max
+    """
     return min((2 * PI * ARM_LENGTH * (abs(initial_position - final_position) / 6.28)) * note_vs_distance_factor,
                MAX_VELOCITY * ARM_LENGTH * half_note_duration)
 
 
 def calculate_angular_velocity(linear_distance, half_note_duration) -> float:
+    """Calcula a velocidade angular à partir do tempo de duração da nota e da distância linear informada.
+
+    :param float linear_distance: distância linear a ser percorrida pelo braço
+    :param float half_note_duration: metade do tempo total de duração da nota
+    :return: velocidade angular a ser setada no motor para que a nota seja tocada corretamente
+    """
     return (linear_distance / half_note_duration) / ARM_LENGTH
 
 
 def get_angular_velocity(total_note_duration, note, sensor_position) -> tuple:
+    """Através de funções auxiliares, calcula a velocidade e posição angulares do motor para a nota recebida
+
+    :param float total_note_duration: tempo total de duração da nota
+    :param int note: nota a ser tocada
+    :param float sensor_position: valor atual do sensor de posição do motor, em rad
+    :return: velocidade e posição angulares do motor para a nota recebida
+    """
     note_vs_distance_factor = calculate_note_vs_distance_factor(note)
     linear_distance = calculate_linear_distance(sensor_position, UP_POSITION, note_vs_distance_factor,
                                                 total_note_duration / 2)
@@ -138,35 +206,45 @@ def get_angular_velocity(total_note_duration, note, sensor_position) -> tuple:
 
 
 def init_robot():
+    """Inicializa o robô, o timestep para o mundo atual e o client de comunicação via OSC Message
+
+    :return: objetos do robô, timestep do robô e client de comunicação via OSC Message
+    """
     robot = Robot()
-    # set the time step of the current world.
     timestep = int(robot.getBasicTimeStep())
-    # set the ip and port to send OSC Messages to SuperCollider
     client = SimpleUDPClient(IP, PORT)
-    # speaker = Speaker("speaker")
     return robot, timestep, client
 
 
 def init_motors(robot):
-    # Insert a getDevice-like function in order to get the
-    # instance of a device of the robot.
+    """Inicializa os motores do robô recebido.
+
+    :param robot: objeto do robô
+    :return: tupla de motores da mão e das juntas do braço
+    """
     hand_motors = [
         robot.getDevice("finger_1_joint_1"),
         robot.getDevice("finger_2_joint_1"),
         robot.getDevice("finger_middle_joint_1")
     ]
 
-    # Rotação vertical (RV) das juntas, iniciando a contagem de baixo para cima
+    # Motores das juntas do braço, iniciando a contagem de baixo (mais longe da mão) para cima (mais prox da mão)
     ur_motors = [
-        robot.getDevice("shoulder_lift_joint"),  # Primeira junta - RV
-        robot.getDevice("elbow_joint"),  # Segunda junta - RV
-        robot.getDevice("wrist_1_joint"),  # Terceira junta - RV
-        robot.getDevice("wrist_2_joint")  # Quarta junta (mão) - Rotação horizontal
+        robot.getDevice("shoulder_lift_joint"),     # Primeira junta - Rotação Vertical
+        robot.getDevice("elbow_joint"),             # Segunda junta - Rotação Vertical
+        robot.getDevice("wrist_1_joint"),           # Terceira junta - Rotação Vertical
+        robot.getDevice("wrist_2_joint")            # Quarta junta (mão) - Rotação horizontal
     ]
     return hand_motors, ur_motors
 
 
 def init_sensors(robot, timestep):
+    """Inicializa os sensores do robô, sendo o de posição especificamente no motor do 'ombro'
+
+    :param robot: objeto do robô
+    :param timestep: timestep para o mundo atual
+    :return: tupla de sensores do robô
+    """
     distance_sensor = robot.getDevice("distance sensor")
     DistanceSensor.enable(distance_sensor, timestep)
 
@@ -180,6 +258,28 @@ def init_sensors(robot, timestep):
 
 
 def play_notes(robot, timestep, client, hand_motors, ur_motors, distance_sensor, touch_sensor, position_sensor):
+    """Após todas as inicializações, entra no loop principal de execução das notas.
+
+    Através dos 5 estados definidos em `status`, utiliza um loop para alternar entre eles e tocar todas as notas.
+    As condições para trocar de estado sempre dizem respeito à posição angular do motor, cujo limite superior varia
+    de nota para nota. Além disso, devido à imprecisão imposta pelo processo mecânico simulado, as verificações de
+    posição não são exatas, sempre havendo uma margem de +-0.1 na posição alvo VS posição atual do motor.
+
+    O comando principal para realização de todas as notas sempre é dado no estado `up command`, onde são sempre
+    calculadas e setadas novas posições alvo e velocidades para o motor. Já o estado `down command` verifica se a nota
+    seguinte será uma pausa. Caso seja, mudará o estado para `waiting pause`, onde permanecerá até que chegue uma nota
+    a ser tocada. Os demais estados funcionam como transição e verificação de posição ou de toque, não possuindo,
+    portanto, nenhuma ação direta no braço do robô.
+
+    :param robot: objeto do robô
+    :param timestep: timestep para o mundo atual
+    :param client: objeto do client de comunicação via OSC Message
+    :param hand_motors: lista contendo todos os motores das mãos
+    :param ur_motors: lista contendo todos os motores das juntas
+    :param distance_sensor: sensor de distância
+    :param touch_sensor: sensor de toque
+    :param position_sensor: sensor de posição referente ao motor do 'ombro'
+    """
     current_status = 0
     notes_counter = 0
     notes = FULL_SONG.copy()
@@ -231,6 +331,8 @@ def play_notes(robot, timestep, client, hand_motors, ur_motors, distance_sensor,
 
 
 def start():
+    """Inicializa os objetos do robô, timestep, client e sensores e chama a função para tocar as notas
+    """
     robot, timestep, client = init_robot()
     hand_motors, ur_motors = init_motors(robot)
     distance_sensor, touch_sensor, position_sensor = init_sensors(robot, timestep)
